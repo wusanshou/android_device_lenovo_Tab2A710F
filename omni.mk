f0bb400ecffd11d113201baa9d3eb508ccaf85f8
Dominique DAMBRAIN <domi@dambrain.fr>
2015-12-02 14:36:42 +0100

    Update omni.mk

--- omni.mk
+++ omni.mk
@@ -13,3 +13,13 @@ PRODUCT_NAME := omni_Tab2A710F
 PRODUCT_BRAND := lenovo
 PRODUCT_MODEL := Tab2A710F
 PRODUCT_MANUFACTURER := lenovo
+
+#Set build fingerprint / ID / Product Name etc.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=Tab2A7-10F \
+    BUILD_FINGERPRINT="Lenovo/Tab2A710F:5.0.2/KTU84P/2015000001:user/release-keys" \
+    PRIVATE_BUILD_DESC="PixNDom_Tab2A710F 5.0.2 KTU84P 2015000001 release-keys"
+
+# Configure as xhdpi device to prevent breaking without mdpi drawables
+PRODUCT_AAPT_CONFIG := normal mdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
