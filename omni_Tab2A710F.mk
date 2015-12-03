270f8830137a8b09cbf93bfd008f9b82f87deaa8
Dominique DAMBRAIN <domi@dambrain.fr>
2015-12-03 18:02:01 +0100

    Create omni_Tab2A710F.mk
    mk file needed by Omnirom

---
+++ omni_Tab2A710F.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := Tab2A710F
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit device configuration (omni specs)
+#$(call inherit-product, device/lenovo/Tab2A710F/device_Tab2A710F.mk)
+$(call inherit-product, device/lenovo/Tab2A710F/Tab2A710F.mk)
+
+#Set build fingerprint / ID / Product Name etc.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=Tab2A710F \
+    BUILD_FINGERPRINT="Lenovo/Tab2A710F:5.0.2/KTU84P/2015000001:user/release-keys" \
+    PRIVATE_BUILD_DESC="PixNDom_Tab2A710F 5.0.2 KTU84P 2015000001 release-keys"
