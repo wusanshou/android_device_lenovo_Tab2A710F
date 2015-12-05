948cceb33a34814eefa16f806cad19b61b4cf26d
Dominique DAMBRAIN <domi@dambrain.fr>
2015-12-05 10:27:22 +0100

    Some cosmetic revision, for lisibility

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,13 @@
-USE_CAMERA_STUB := true
+# BoardConfig.mk for Lenovo Tab2 A7-10F (Omnirom)
 
 # inherit from the proprietary version
 -include vendor/lenovo/Tab2A710F/BoardConfigVendor.mk
 
+################################ Define cross compiler configuration
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
-#TARGET_BOARD_PLATFORM := unknown
+TARGET_BOARD_PLATFORM := mt8127
+TARGET_BOARD_PLATFORM_GPU := mali-450
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
@@ -13,62 +15,60 @@ TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
+################################# Define Flash topography 
+
+# ToDo : see if this parameter can be changed to match the Lenovo's name of Board
 TARGET_BOOTLOADER_BOARD_NAME := Tab2A710F
 
-BOARD_KERNEL_CMDLINE := 		# pas teste. d'apres la config du boot
-BOARD_KERNEL_BASE := 0x80000000		# teste. d'apres la config du boot. pas de boot...
-#BOARD_KERNEL_BASE := 0x01D80000	# teste, reboot auto au bout de qqes secondes. d'apres le scatter
-BOARD_KERNEL_PAGESIZE := 0x00000800	# pas teste. d'apres la config du boot
-BOARD_KERNEL_OFFSET := 0x00008000	# pas teste. d'apres la config du boot
-BOARD_RAMDISK_OFFSET := 0x04000000	# pas teste. d'apres la config du boot
-BOARD_TAGS_OFFSET := 0x00000100		# pas teste. d'apres la config du boot
+BOARD_KERNEL_CMDLINE :=               # pas teste. d'apres la config du boot
+BOARD_KERNEL_BASE := 0x80000000			  # teste. d'apres la config du boot...
+BOARD_KERNEL_PAGESIZE := 0x00000800		# = 2048
+BOARD_KERNEL_OFFSET := 0x00008000		  # pas teste. d'apres la config du boot
+BOARD_RAMDISK_OFFSET := 0x04000000	  # pas teste. d'apres la config du boot
+BOARD_TAGS_OFFSET := 0x00000100		    # pas teste. d'apres la config du boot
 
 # fix this up by examining /proc/mtd on a running device
-#BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216 #0x1000000
-#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216 #0x1000000
-#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736 #0x60000000
-#BOARD_USERDATAIMAGE_PARTITION_SIZE := 3221225472 #0xc0000000
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 3221225472
-
-
 BOARD_FLASH_BLOCK_SIZE := 131072	# BOARD_KERNEL_PAGESIZE * 64 trouve sur un forum, valeur generee par CM
+
 BOARD_USES_UBOOT := true		# pas sur mais probable
-#BOARD_KERNEL_IMAGE_NAME := uImage	# not usable with Omnirom
 
 BOARD_HAS_LARGE_FILESYSTEM := true	# pas sur, utile ?
 TARGET_USERIMAGES_USE_EXT4 := true
 GET_USERIMAGES_SPARSE_EXT_DISABLED := false
 
-TARGET_BOARD_PLATFORM := mt8127
-TARGET_BOARD_PLATFORM_GPU := mali-450
-#TARGET_PREBUILT_KERNEL := device/lenovo/Tab2A710F/prebuilt/kernel  # Use this to reuse a pre-built kernel
-TARGET_KERNEL_CONFIG := bitland8127_tb_l_defconfig		  # config file located in kernel/lenovo/a710f/arch/arm/configs
-#TARGET_KERNEL_CONFIG := bitland8127_tb_l_debug_defconfig  #the same in debug mode, if needed
+############################## Kernel parameters to use ##########
+# activate following parameter to use a prebuilt kernel
+#TARGET_PREBUILT_KERNEL := device/lenovo/Tab2A710F/prebuilt/kernel  
 
-# Recovery
+# activate following parameter to build the normal kernel
+# config file located in kernel/lenovo/Tab2A710F/arch/arm/configs/
+TARGET_KERNEL_CONFIG := bitland8127_tb_l_defconfig		  
+
+# activate following parameter to build a debug kernel
+#TARGET_KERNEL_CONFIG := bitland8127_tb_l_debug_defconfig
+###################################################################
+
+# Recovery (TWRP)
 TARGET_RECOVERY_FSTAB := device/lenovo/Tab2A710F/recovery/recovery.fstab
 RECOVERY_FSTAB_VERSION := 2
 #TARGET_PREBUILT_RECOVERY_KERNEL := device/lenovo/Tab2A710F/recovery/kernel
 RECOVERY_SDCARD_ON_DATA := true		# xda TWRP
-#TARGET_PREBUILT_RECOVERY_KERNEL := path/to/kernel
 
-
-# Ajouts divers...
+# Some additional features...
+USE_CAMERA_STUB := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 USE_OPENGL_RENDERER := true
 DEVICE_RESOLUTION := 1024x600
 BOARD_HAVE_BLUETOOTH := true
 
-# depuis le pack Lenovo KitKat
-#USE_CAMERA_STUB := true
+# From Lenovo's KitKat OSC package
 TARGET_NO_FACTORYIMAGE := true
 TARGET_KMODULES := true
 BOARD_USES_MTK_AUDIO := true
 BOARD_EGL_CFG := device/lenovo/Tab2A710F/prebuilt/etc/egl.cfg
 BUILD_EMULATOR_OPENGL := true
 BUILD_EMULATOR_OPENGL_DRIVER := true
-
-
