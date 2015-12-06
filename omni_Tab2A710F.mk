70639378d4c58a18f467c0134ed7eab12c86c89e
Dominique DAMBRAIN <domi@dambrain.fr>
2015-12-06 13:13:17 +0100

    Update omni_Tab2A710F.mk
    Use build.id = KOT49H , like adaml for stock KitKat

--- omni_Tab2A710F.mk
+++ omni_Tab2A710F.mk
@@ -11,5 +11,5 @@ $(call inherit-product, device/lenovo/Tab2A710F/Tab2A710F.mk)
 #Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=Tab2A710F \
-    BUILD_FINGERPRINT="Lenovo/Tab2A710F:5.0.2/KTU84P/2015000001:user/release-keys" \
-    PRIVATE_BUILD_DESC="PixNDom_Tab2A710F 5.0.2 KTU84P 2015000001 release-keys"
+    BUILD_FINGERPRINT="Lenovo/Tab2A710F:5.0.2/KOT49H/2015000001:user/release-keys" \
+    PRIVATE_BUILD_DESC="PixNDom_Tab2A710F 5.0.2 KOT49H 2015000001 release-keys"
