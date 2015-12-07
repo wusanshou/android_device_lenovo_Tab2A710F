f924f6e6e7474a660098ea25180a01d0a0d68a4e
Dominique DAMBRAIN <domi@dambrain.fr>
2015-12-07 10:54:39 +0100

    Set BUILD_ID in accordance with 5.1 repository

--- omni_Tab2A710F.mk
+++ omni_Tab2A710F.mk
@@ -14,7 +14,8 @@ $(call inherit-product, device/lenovo/Tab2A710F/Tab2A710F.mk)
 #Set build fingerprint / ID / Product Name etc.
 # See this page for BUILD_ID value : 
 # https://source.android.com/source/build-numbers.html#source-code-tags-and-builds
+# Build_id = LYZ28J for Lollipop 5.1
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=Tab2A710F \
-    BUILD_FINGERPRINT="Lenovo/Tab2A710F:5.0.2/LRX22G/2015000001:user/release-keys" \
-    PRIVATE_BUILD_DESC="PixNDom_Tab2A710F 5.0.2 LRX22G 2015000001 release-keys"
+    BUILD_FINGERPRINT="Lenovo/Tab2A710F:5.0.2/LYZ28J/2015000001:user/release-keys" \
+    PRIVATE_BUILD_DESC="PixNDom_Tab2A710F 5.0.2 LYZ28J 2015000001 release-keys"
