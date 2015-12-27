ed27186b15a4538c5d569c3ce7489a88372c5c69
Sébastien Bordenave <sbordenave@gmail.com>
2015-12-27 13:55:53 +0100

    modifié :         BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 3221225472	# = 3 GB sur 5,5 GB max  (OK wi
 BOARD_FLASH_BLOCK_SIZE := 0x020000			# BOARD_KERNEL_PAGESIZE * 64 trouve sur un forum, valeur generee par CM
 
 # Needed for Android 6 ###########################
-BOARD_CACHEIMAGE_PARTITION_SIZE := 132120576		# = 0x07e00000 = 128MB	# Pix : ça me semble petit comme valeur...
+BOARD_CACHEIMAGE_PARTITION_SIZE := 132120576		# = 0x07e00000 = 128MB
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true		# semble ok avec Omnirom 5.1.1 maintenant
 ##################################################
